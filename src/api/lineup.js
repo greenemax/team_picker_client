04e6f495d9f1ace9051414c06f0b5e7cdbc39f4a
greenemax <mjgreene27@gmail.com>
2020-06-22 19:46:08 -0400

    first shot at api js for lineup and players.

---
+++ src/api/lineup.js
@@ -0,0 +1,45 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createEmptyLineup = user => {
+  return axios({
+    url: apiUrl + '/lineup',
+    method: 'PATCH',
+    data: {
+      'lineup': {
+        'players': [],
+        'user': `${user._id}`,
+        'active': true,
+        'totalCost': 0,
+        'stripeId': null
+      }
+    },
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const getHistory = user => {
+  return axios({
+    url: apiUrl + '/lineup',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const changeCartActive = (user, id, boolean) => {
+  return axios({
+    url: apiUrl + `/lineup/${id}/active`,
+    method: 'PATCH',
+    data: {
+      'lineup': {
+        'active': boolean
+      }
+    },
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
---
+++ src/api/players.js
@@ -0,0 +1,40 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const getPlayers = () => {
+  return axios({
+    url: apiUrl + '/players'
+  })
+}
+
+export const getOnePlayer = id => {
+  return axios({
+    url: apiUrl + '/players/' + id
+  })
+}
+
+export const addToLineup = (id, player, user) => {
+  return axios({
+    url: apiUrl + '/lineup/' + id,
+    method: 'PATCH',
+    data: {
+      'player': player
+    },
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const removeFromLineup = (id, player, user) => {
+  return axios({
+    url: apiUrl + `/shopping-cart/${id}/products`,
+    method: 'PATCH',
+    data: {
+      'player': player
+    },
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
